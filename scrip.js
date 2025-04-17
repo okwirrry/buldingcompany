ffb59eef8786d22c634bbcbf56347043420132ff
okwirrry <fredrickokwiri30@gmail.com>
2025-04-17 15:56:06 +0300

    Update scrip.js

--- scrip.js
+++ scrip.js
@@ -1,29 +1,26 @@
-function submitdata(){
-const form = document.getElementById("myform");
-  form.addEventListener("submit", function(e) {
-    e.preventDefault();
+document.getElementById("myform").addEventListener("submit", function(e) {
+  e.preventDefault();
 
-    const data = {
-      FirstName: document.getElementById("FirstName").value,
-      LastName: document.getElementById("LastName").value,
-      PhoneNumber: document.getElementById("PhoneNumber").value,
-      Email: document.getElementById("Email").value,
-      Message: document.getElementById("Message").value,
-    };
+  const data = {
+    FirstName: document.getElementById("FirstName").value,
+    LastName: document.getElementById("LastName").value,
+    PhoneNumber: document.getElementById("PhoneNumber").value,
+    Email: document.getElementById("Email").value,
+    Message: document.getElementById("Message").value,
+  };
 
-    fetch("https://script.google.com/macros/s/AKfycbyRPhPPm9t8RJzVsivlPyysW6-inlpdWbRINkW7vtblu5YwLUtlhIATfnvqJZACODF8/exec", {
-      method: "POST",
-      body: JSON.stringify(data),
-      headers: { "Content-Type": "application/json" },
-    })
-    .then(response => response.json())
-    .then(response => {
-      alert("Message sent successfully!");
-      form.reset();
-    })
-    .catch(error => {
-      console.error("Error!", error.message);
-      alert("Something went wrong.");
-    });
+  fetch("https://script.google.com/macros/s/AKfycbyRPhPPm9t8RJzVsivlPyysW6-inlpdWbRINkW7vtblu5YwLUtlhIATfnvqJZACODF8/exec", {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: { "Content-Type": "application/json" },
+  })
+  .then(response => response.json())
+  .then(response => {
+    alert("Message sent successfully!");
+    document.getElementById("myform").reset();
+  })
+  .catch(error => {
+    console.error("Error!", error.message);
+    alert("Something went wrong.");
   });
-}
+});
